b55c70b6e594e8132c38bd5556e6cb1cf0514f64
Carlos Alberto Arrieta <merecmoney3@gmail.com>
2020-03-22 05:53:22 -0600

    Remove unnecessary column when getting table references
     Remove TABLESPACE_NAME column

--- s-04-CAHA-consulta-fragmentos.sql
+++ s-04-CAHA-consulta-fragmentos.sql
@@ -5,10 +5,10 @@ set linesize 2000
 Prompt Conectando a S1 - cahabdd_s1
 connect bancos_bdd/bancos_bdd@cahabdd_s1
 Prompt mostrando lista de fragmentos para s1
-select TABLE_NAME, TABLESPACE_NAME from USER_TABLES ORDER BY TABLE_NAME;
+select TABLE_NAME from USER_TABLES ORDER BY TABLE_NAME;
 Prompt Conectando a S2 - cahabdd_s2
 connect bancos_bdd/bancos_bdd@cahabdd_s2
 Prompt mostrando lista de fragmentos para s2
-select TABLE_NAME, TABLESPACE_NAME from USER_TABLES ORDER BY TABLE_NAME;
+select TABLE_NAME from USER_TABLES ORDER BY TABLE_NAME;
 Prompt Listo!
 exit
