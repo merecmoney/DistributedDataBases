deabe09f60670509bb2186281596592cb50ef42b
Carlos Alberto Arrieta <merecmoney3@gmail.com>
2020-06-02 17:29:35 -0500

    Add script for database links
     - This database links are to manage materialized views

---
+++ Practice_6/s_03_CAHA_creacion_ligas.sql
@@ -0,0 +1,12 @@
+--@Autor: Hernández Arrieta Carlos Alberto
+--@Fecha creación: 1/Junio/2020
+--@Descripción: Creación de ligas para la base de replicación
+prompt Creando liga en cahabdd_s2 hacia cahabdd_s1
+connect caha_replica_bdd/caha_replica_bdd@cahabdd_s2
+--liga administrativa
+create public database link cahabdd_s1_master.fi.unam using 'CAHABDD_S1';
+--liga privada, no requiere hacer uso de USING
+create database link cahabdd_s1_master.fi.unam connect to caha_replica_bdd
+identified by caha_replica_bdd;
+
+exit
