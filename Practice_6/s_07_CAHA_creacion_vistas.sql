1e740304da55940be2df8156279beadf1229a076
Carlos Alberto Arrieta <merecmoney3@gmail.com>
2020-06-04 09:04:11 -0500

    Add comments to s_07_CAHA_creacion_vistas.sql

--- Practice_6/s_07_CAHA_creacion_vistas.sql
+++ Practice_6/s_07_CAHA_creacion_vistas.sql
@@ -3,11 +3,13 @@
 --@Descripción: Creación de vistas materializadas para la base
 -- de datos de replicación.
 
+prompt conectando a cahabdd_s1
 connect caha_replica_bdd/caha_replica_bdd@cahabdd_s1
 
 prompt Creación de mlogs
 @s_06_CAHA_definicion_mlogs.sql
 
+prompt conectando a cahabdd_s2
 connect caha_replica_bdd/caha_replica_bdd@cahabdd_s2
 
 prompt Creación de vistas materializadas
